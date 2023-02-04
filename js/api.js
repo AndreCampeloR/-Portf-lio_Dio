45a0ff9f43f603c1865bb9ed4246d7245c466213
André Campelo <decocampeloreis@gmail.com>
2023-02-04 19:07:55 -0300

    atualizando api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/Portfolio_Dio/blob/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/AndreCampeloR/Portfolio_Dio/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
